0ab6afb5a124240ad4bc1032938720332b8aafe0
honey0632 <honey603020@gmail.com>
2024-01-20 22:15:10 +0530

    jan 20

---
+++ 01-20-2024.cpp
@@ -0,0 +1,40 @@
+// 
+// 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct Node {
+	int key;
+	Node *left, *right;
+};
+
+
+class Solution
+{
+    public:
+    int solve(Node *root, int &ans)
+    {
+        if(!root) return 0;
+
+        int left=solve(root->left, ans);
+        int right=solve(root->right, ans);
+        
+        ans+=abs(left)+abs(right);
+        int extraCandies = root->key + left + right - 1;
+        return extraCandies;
+    }
+    int distributeCandy(Node* root)
+    {
+        //code here
+        int ans=0;
+        solve(root,ans);
+        return ans;
+    }
+};
+
+int main () {
+  
+   return 0;
+}
