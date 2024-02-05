bd284cdf707392f6468bf968233c0b230ced7ffb
honey0632 <honey603020@gmail.com>
2024-02-05 23:53:54 +0530

    feb 05

---
+++ 02-05-2024.cpp
@@ -0,0 +1,55 @@
+// Sorted insert for circular linked list
+// https://www.geeksforgeeks.org/problems/sorted-insert-for-circular-linked-list/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+  int data;
+  struct Node *next;
+  
+  Node(int x){
+      data = x;
+      next = NULL;
+  }
+  
+};
+
+ 
+// This function should return head of
+// the modified list
+class Solution
+{
+    public:
+    Node *sortedInsert(Node* head, int data)
+    {
+        Node* nn=new Node(data);
+       if(head==NULL){
+           nn->next=nn;
+           return nn;
+       }
+       
+       if(head->data>=data){
+           nn->next=head;
+           Node* temp=head;
+           while(temp->next!=head){
+               temp=temp->next;
+           }
+           temp->next=nn;
+           return nn;
+       }
+       Node* temp=head;
+       while(temp->next!=head && temp->next->data<data){
+                 
+                   temp=temp->next;}
+       nn->next=temp->next;
+       temp->next=nn;
+       return head;
+    }
+};
+
+int main () {
+  
+   return 0;
+}
