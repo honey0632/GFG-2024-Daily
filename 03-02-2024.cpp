32f90c8a7c56c8d2c96863a019fdc2e1b68cfb18
honey0632 <honey603020@gmail.com>
2024-03-02 17:16:43 +0530

    mar 02

---
+++ 03-02-2024.cpp
@@ -0,0 +1,24 @@
+// First element to occur k times
+// https://www.geeksforgeeks.org/problems/first-element-to-occur-k-times5150/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int firstElementKTime(int n, int k, int a[])
+    {   
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[a[i]]++;
+            if(mp[a[i]]==k) return a[i];
+        }
+        return -1;
+    }
+};
+
+int main () {
+    
+     return 0;
+}
