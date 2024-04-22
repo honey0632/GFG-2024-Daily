ac4b04f7893e846fa555914d8d608e8df5c987ed
honey0632 <honey603020@gmail.com>
2024-04-22 18:40:08 +0530

    apr 22

---
+++ 04-22-2024.cpp
@@ -0,0 +1,29 @@
+// Row with minimum number of 1's
+// https://www.geeksforgeeks.org/problems/row-with-minimum-number-of-1s5430/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int minRow(int n, int m, vector<vector<int>> a) {
+        int ans=0,mini=INT_MAX;
+        int count=0;
+        for(int i=0;i<n;i++) {
+            count=0;
+            for(int j=0;j<m;j++) {
+                if(a[i][j]==1) count++;
+            }
+            if(count<mini) {
+                mini = count;
+                ans = i;
+            }
+        }
+        return ans+1;
+    }
+};
+
+int main () {
+    
+     return 0;
+}
