5c63584845407f62589da3c70470f51fa144e5d6
honey0632 <honey603020@gmail.com>
2024-05-09 12:58:16 +0530

    may 09

---
+++ 05-09-2024.cpp
@@ -0,0 +1,18 @@
+// Divisor Game
+// https://www.geeksforgeeks.org/problems/divisor-game-1664432414/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool divisorGame(int n) {
+        return (n%2==0); 
+         
+    }
+};
+
+int main () {
+    
+     return 0;
+}
