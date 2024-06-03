aa1244edc06a2b45028612867b14d5795773998f
honey0632 <honey603020@gmail.com>
2024-06-03 21:08:36 +0530

    jun 03

---
+++ 06-03-2024.cpp
@@ -0,0 +1,26 @@
+// Trail of ones
+// https://www.geeksforgeeks.org/problems/trail-of-ones3242/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int numberOfConsecutiveOnes(int n) {
+        // code here
+        if(n==2)return 1;
+        int a=0,b=1,ans=1,mod=1e9+7;
+        for(int i=3; i<=n; i++){
+            int c=(a+b)%mod;
+            a=b;
+            b=c;
+            ans=((ans*2)%mod+c)%mod;
+        }
+        return ans;
+    }
+};
+
+int main () {
+    
+     return 0;
+}
