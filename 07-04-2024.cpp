215ce68ee47fac7f3700fd16064caa505429d97a
honey0632 <honey603020@gmail.com>
2024-07-04 22:32:43 +0530

    jul 04

---
+++ 07-04-2024.cpp
@@ -0,0 +1,48 @@
+// Duplicate Subtrees
+// https://www.geeksforgeeks.org/problems/duplicate-subtrees/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    string data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+class Solution {
+  public:
+    string helper(Node *root, vector<Node*> &ans, 
+                unordered_map<string,int> &mp)
+    {
+        string temp = "";
+        if(root == NULL)
+            return "#";
+        temp += to_string(root->data);
+        temp += helper(root->left,ans,mp);
+        temp += helper(root->right,ans,mp);
+        if(mp[temp] == 1)
+            ans.push_back(root);
+        mp[temp]++;
+        return temp;
+        
+    }
+    vector<Node*> printAllDups(Node* root) 
+    {
+        vector<Node*> ans;
+        unordered_map<string,int> mp;
+        helper(root,ans,mp);
+        return ans;
+        // Code here
+    }
+};
+
+int main () {
+    
+     return 0;
+}
