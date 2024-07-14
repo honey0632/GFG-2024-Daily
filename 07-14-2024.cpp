a667d662aed9b3e90c24be69681948680b2ead3e
honey0632 <honey603020@gmail.com>
2024-07-14 17:34:45 +0530

    jul 14

---
+++ 07-14-2024.cpp
@@ -0,0 +1,19 @@
+// Segregate 0s and 1s
+// https://www.geeksforgeeks.org/problems/segregate-0s-and-1s5106/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        
+        sort(arr.begin(),arr.end());
+        
+    }
+};
+
+int main () {
+    
+     return 0;
+}
