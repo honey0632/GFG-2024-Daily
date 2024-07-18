7ef661d77426b2fcb643d201dbd6232b20d9e9bf
honey0632 <honey603020@gmail.com>
2024-07-18 20:05:33 +0530

    jul 18

---
+++ 07-18-2024.cpp
@@ -0,0 +1,23 @@
+// Longest alternating subsequence
+// https://www.geeksforgeeks.org/problems/longest-alternating-subsequence5951/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+     int alternatingMaxLength(vector<int>& arr) {
+        int inc = 1;
+        int dec = 1;
+        for(int p = 1; p < arr.size(); p++){
+            if(arr[p] > arr[p-1]) inc = dec+1;
+            else if(arr[p]<arr[p-1]) dec = inc+1;
+        }
+        return max(inc, dec);
+    }
+};
+
+int main () {
+    
+     return 0;
+}
