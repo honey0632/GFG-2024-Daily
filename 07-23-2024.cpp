18ba0b5111fab61786b752d1fd103a3273fad489
honey0632 <honey603020@gmail.com>
2024-07-23 20:26:20 +0530

    jul 23

---
+++ 07-23-2024.cpp
@@ -0,0 +1,64 @@
+// Merge two BST 's
+// https://www.geeksforgeeks.org/problems/merge-two-bst-s/1
+ 
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+class Solution {
+  public:
+    void inorder(Node* root, vector<int>& vec){
+        if(root==NULL)
+            return;
+        inorder(root->left, vec);
+        vec.push_back(root->data);
+        inorder(root->right, vec);
+    }
+    // Function to return a list of integers denoting the node
+    // values of both the BST in a sorted order.
+    vector<int> merge(Node *root1, Node *root2) {
+        // Your code here
+        vector<int> bs1;
+        vector<int> bs2;
+        vector<int> ans;
+        inorder(root1, bs1);
+        inorder(root2, bs2);
+        int n = bs1.size();
+        int m = bs2.size();
+        int i =0, j = 0;
+        while(i<n && j<m){
+            if(bs1[i] > bs2[j]){
+                ans.push_back(bs2[j]);
+                j++;
+            }
+            else{
+                ans.push_back(bs1[i]);
+                i++;
+            }
+        }
+        while(i<n){
+            ans.push_back(bs1[i]);
+            i++;
+        }
+        while(j<m){
+            ans.push_back(bs2[j]);
+            j++;
+        }
+        return ans;
+    }
+};
+
+int main () {
+    
+     return 0;
+}
