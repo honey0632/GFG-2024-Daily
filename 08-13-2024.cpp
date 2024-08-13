755ac1139a17b5d27ed8b231863a26c51d2c5837
honey0632 <honey603020@gmail.com>
2024-08-13 22:48:13 +0530

    aug 13

---
+++ 08-13-2024.cpp
@@ -0,0 +1,18 @@
+// Square root of a number
+// https://www.geeksforgeeks.org/problems/square-root/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    long long int floorSqrt(long long int n) {
+        // Your code goes here
+        return floor(sqrt(n));
+    }
+};
+
+int main () {
+    
+     return 0;
+}
