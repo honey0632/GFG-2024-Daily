6ee0c100af7c2416516174f436b84ad9ce719c95
honey0632 <honey603020@gmail.com>
2024-09-04 17:58:52 +0530

    sep 04

---
+++ 09-04-2024.cpp
@@ -0,0 +1,17 @@
+// Count ways to N'th Stair(Order does not matter)
+// https://www.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter5639/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+	public:
+		int nthStair(int n){
+		    return n/2 +1;
+		}
+};
+
+int main () {
+    
+     return 0;
+}
