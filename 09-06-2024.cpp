49f524d9b31eab0d721b7e43edc2e0f5d08dbfbe
honey0632 <honey603020@gmail.com>
2024-09-06 23:55:42 +0500

    sep 06

---
+++ 09-06-2024.cpp
@@ -0,0 +1,32 @@
+// Kadane's Algorithm
+// https://www.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(vector<int>arr){
+        
+        int ans=INT_MIN, n = arr.size();
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            ans=max(ans,sum);
+            if(sum<0) sum=0;
+        }
+        
+        return ans;
+        
+    }
+};
+
+
+int main () {
+    
+     return 0;
+}
+
+
