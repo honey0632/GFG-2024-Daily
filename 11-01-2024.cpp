05216999d50e25bcb6883d0cd9da0c82a541b9da
honey0632 <honey603020@gmail.com>
2024-11-01 22:31:30 +0530

    nov 01

--- 11-01-2024.cpp
+++ 11-01-2024.cpp
@@ -1,27 +1,27 @@
-// 1957. Delete Characters to Make Fancy String
-// https://leetcode.com/problems/delete-characters-to-make-fancy-string/
+// Swap and Maximize
+// https://www.geeksforgeeks.org/problems/swap-and-maximize5859/1 
+
 
 #include<bits/stdc++.h>
 using namespace std;
-
 class Solution {
-public:
-    string makeFancyString(string s) {
-        string ans = "";
-        ans.push_back(s[0]);
-        int n = s.size(),cnt = 1;
-        for(int i=1;i<n;i++){
-            if(s[i] == ans.back()){
-                cnt++;
-                if(cnt < 3)ans.push_back(s[i]);
-            }
-            else{
-                cnt = 1;
-                ans.push_back(s[i]);
-            }
+  public:
+    long long maxSum(vector<int>& arr) {
+        // code here
+        long long res=0;
+        int n=arr.size();
+        //maximum absolute difference when alternate maximum and minimum element permuted
+        sort(arr.begin(),arr.end());
+        int i=0,j=n-1;
+        while(i<j){
+            res+=abs(arr[j]-arr[i])+abs(arr[j]-arr[i+1]);
+            i++;
+            j--;
         }
-        return ans;
-    }
+        //given it is circular array
+        res+=abs(arr[i]-arr[0]);
+       return res;
+        }
 };
 
 int main() {
