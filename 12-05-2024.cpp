97d77c9718fb3ab79ce79bef9dbfd768e6638096
honey0632 <honey603020@gmail.com>
2024-12-05 21:29:39 +0530

    dec 05

---
+++ 12-05-2024.cpp
@@ -0,0 +1,30 @@
+// Sort 0s, 1s and 2s
+// https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void sort012(vector<int>& a) {
+        // code here
+        int low=0,mid = 0;
+        int high = a.size()-1;
+        while(mid<=high){
+            if(a[mid]==0){
+                swap(a[low++],a[mid++]);
+            }
+            else if(a[mid]==2){
+                swap(a[mid],a[high--]);
+            }
+            else{
+                mid++;
+            }
+        }
+    }
+};
+
+int main() {
+    
+     return 0;
+}
