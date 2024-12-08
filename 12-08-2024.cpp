caf534fab15a1094e4cb0a9c4ef65c1fe933b9ff
honey0632 <honey603020@gmail.com>
2024-12-08 23:58:07 +0530

    dec 08

---
+++ 12-08-2024.cpp
@@ -0,0 +1,28 @@
+// Overlapping Intervals
+// https://www.geeksforgeeks.org/problems/overlapping-intervals--170633/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
+        // Code here
+        vector<vector<int>>ans;
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size();i++){
+            if(ans.empty() || ans.back()[1]<arr[i][0]){
+                ans.push_back(arr[i]);
+            }
+            else{
+                ans.back()[1]=max(ans.back()[1],arr[i][1]);
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    
+     return 0;
+}
