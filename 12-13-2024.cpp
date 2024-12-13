044b578497aad2f3564b0eab82e06c7821e25422
honey0632 <honey603020@gmail.com>
2024-12-13 20:35:18 +0530

    dec 13

--- 12-13-2024.cpp
+++ 12-13-2024.cpp
@@ -1,42 +1,30 @@
-// 2593. Find Score of an Array After Marking All Elements
-// https://leetcode.com/problems/find-score-of-an-array-after-marking-all-elements/
+// Sorted and Rotated Minimum
+// https://www.geeksforgeeks.org/problems/minimum-element-in-a-sorted-and-rotated-array3611/1
 
 #include<bits/stdc++.h>
 using namespace std;
 
 class Solution {
-public:
-    long long findScore(vector<int>& nums) {
-        int n = nums.size();
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;  // Min-heap based on value
-
-        // Build the min-heap with (value, index) pairs
-        for (int i = 0; i < n; i++) {
-            minHeap.push({nums[i], i});
-        }
-
-        long long score = 0;  // Use long long to handle overflow
-
-        // Process elements in min-heap
-        while (!minHeap.empty()) {
-            auto curr = minHeap.top();
-            minHeap.pop();
-            int num = curr.first;
-            int idx = curr.second;
-
-            if (nums[idx] != -1) {  // Process only if not already marked
-                score += num;
-                nums[idx] = -1;  // Mark the current index
-                if (idx > 0 && nums[idx - 1] != -1) {
-                    nums[idx - 1] = -1;  // Mark the left neighbor
-                }
-                if (idx < n - 1 && nums[idx + 1] != -1) {
-                    nums[idx + 1] = -1;  // Mark the right neighbor
-                }
+  public:
+    int findMin(vector<int>& arr) {
+        // complete the function here
+        int n=arr.size();
+        int low=0, high=n-1;
+        int ans=arr[low];
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]<ans) ans = arr[mid];
+            if(arr[mid]>=arr[low]){
+                ans = min(ans,arr[low]);
+                low=mid+1;
             }
+            else{
+                ans=min(ans,arr[mid]);
+                high=mid-1;
+            }
+            
         }
-
-        return score;
+        return ans;
     }
 };
 
