cc770c8cdb0265a668a151bb9da4ab156ef2717e
honey0632 <honey603020@gmail.com>
2024-12-16 19:10:20 +0530

    dec 16

---
+++ 12-16-2024.cpp
@@ -0,0 +1,22 @@
+// K-th element of two Arrays
+// https://www.geeksforgeeks.org/problems/k-th-element-of-two-sorted-array1317/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int kthElement(vector<int>& arr1, vector<int>& arr2, int k) {
+        // code here
+        for(int i=0;i<arr2.size();i++){
+            arr1.push_back(arr2[i]);
+        }
+        sort(arr1.begin(),arr1.end());
+        return arr1[k-1];
+    }
+};
+
+int main() {
+    
+     return 0;
+}
